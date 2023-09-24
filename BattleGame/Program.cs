54486e925a24f1d7cae1c509f54f44e4e229eb89
Huyvictory <vominhhuy008@gmail.com>
2023-09-24 13:39:49 +0700

    Add 2 folder codes for while-do while loop excercises

---
+++ BattleGame/Program.cs
@@ -0,0 +1,39 @@
+﻿int heroHealthPoint = 10;
+int monsterHealthPoint = 10;
+
+string winner = "";
+
+Random randomAttackPoint = new Random();
+do
+{
+    //Hero turn
+    int randomAttackPointHero = randomAttackPoint.Next(1, 11);
+
+    monsterHealthPoint -= randomAttackPointHero;
+
+    Console.WriteLine(
+        $"Monster was damaged and lost {randomAttackPointHero} health and now has {monsterHealthPoint}"
+    );
+    if (monsterHealthPoint <= 0)
+    {
+        winner = "Hero";
+        break;
+    }
+
+    //Monster turn
+    //Hero turn
+    int randomAttackPointMonster = randomAttackPoint.Next(1, 11);
+
+    heroHealthPoint -= randomAttackPointMonster;
+
+    Console.WriteLine(
+        $"Hero was damaged and lost {randomAttackPointMonster} health and now has {heroHealthPoint}"
+    );
+    if (heroHealthPoint <= 0)
+    {
+        winner = "Monster";
+        break;
+    }
+} while (heroHealthPoint > 0 && monsterHealthPoint > 0);
+
+Console.WriteLine($"{winner} wins!");
---
+++ While-DoWhile-Excercises/Program.cs
@@ -0,0 +1,97 @@
+﻿//***********************************************
+
+// Ex 1: Write code that validates integer input
+
+// Console.WriteLine("Please type number from 5 to 10");
+
+// int userInput = 0;
+// while (userInput < 5 || userInput > 10)
+// {
+//     bool isValidUserInput = int.TryParse(Console.ReadLine()?.Trim(), out userInput);
+
+//     if (!isValidUserInput)
+//     {
+//         Console.WriteLine("Your input was invalid please try again!");
+//     }
+//     else
+//     {
+//         if (userInput < 5 || userInput > 10)
+//         {
+//             Console.WriteLine("Your input was out of range. Try again!");
+//         }
+//     }
+// }
+
+// Console.WriteLine($"Your input was {userInput}");
+
+//***********************************************
+
+//***********************************************
+
+// Ex 2: Validate string input
+
+// Console.WriteLine("Please specify your role : (Administrator | Manager | User)");
+
+// string userInputRole = "";
+
+// do
+// {
+//     userInputRole = Console.ReadLine();
+
+//     if (String.IsNullOrWhiteSpace(userInputRole))
+//     {
+//         Console.WriteLine("Your input was invalid, please try again");
+//     }
+//     else
+//     {
+//         string processedUserInputRole = userInputRole.Trim().ToLower();
+//         if (
+//             processedUserInputRole == "Administrator".ToLower()
+//             || processedUserInputRole == "Manager".ToLower()
+//             || processedUserInputRole == "User".ToLower()
+//         )
+//         {
+//             break;
+//         }
+//         else
+//         {
+//             Console.WriteLine(
+//                 $"The role name that you entered, \"{userInputRole}\" is not valid. Try again!"
+//             );
+//         }
+//     }
+// } while (true);
+
+// Console.WriteLine($"Your input {userInputRole} was accepted");
+
+//***********************************************
+
+//***********************************************
+
+// Ex 3:
+string[] myStrings = new string[2]
+{
+    "I like pizza. I like roast chicken. I like salad",
+    "I like all three of the menu choices"
+};
+
+foreach (string myString in myStrings)
+{
+    if (myString.IndexOf('.') != -1)
+    {
+        string[] contentsToDisplay = myString.Split('.');
+        int contentNumberToDisplay = 0;
+
+        while (contentNumberToDisplay < contentsToDisplay.Length)
+        {
+            Console.WriteLine($"{contentsToDisplay[contentNumberToDisplay].Trim()}");
+            contentNumberToDisplay++;
+        }
+    }
+    else
+    {
+        Console.WriteLine(myString);
+    }
+}
+
+//***********************************************
