b06cabedbe944a8e755e723e812e1917652f1955
Huyvictory <vominhhuy008@gmail.com>
2023-09-25 23:33:48 +0700

    Add 2 folder codes DatatypeConversion and exercises in ReverseWordMessage of part 4

---
+++ ReverseWordMessage/Program.cs
@@ -0,0 +1,36 @@
+﻿// Ex 1:
+// string pangram = "The quick brown fox jumps over the lazy dog";
+
+// string[] splittedPangrams = pangram.Split(" ");
+
+// for (int i = 0; i < splittedPangrams.Length; i++)
+// {
+//     char[] splittedPangram = splittedPangrams[i].ToCharArray();
+
+//     Array.Reverse(splittedPangram);
+
+//     splittedPangrams[i] = new string(splittedPangram);
+// }
+
+// pangram = string.Join(" ", splittedPangrams);
+
+// Console.WriteLine(pangram);
+
+// Ex 2:
+string orderStream = "B123,C234,A345,C15,B177,G3003,C235,B179";
+
+string[] orderIds = orderStream.Split(',');
+
+Array.Sort(orderIds);
+
+foreach (string orderId in orderIds)
+{
+    if (orderId.Length != 4)
+    {
+        Console.WriteLine($"{orderId} - Error");
+    }
+    else
+    {
+        Console.WriteLine(orderId);
+    }
+}
