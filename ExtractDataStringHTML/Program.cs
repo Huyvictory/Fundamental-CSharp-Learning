21885f3624d386d16fd68f4a6b93da6062ea9748
Huyvictory <vominhhuy008@gmail.com>
2023-09-27 00:25:18 +0700

    Add another 2 folder codes for c# trainning course part 4

---
+++ ExtractDataStringHTML/Program.cs
@@ -0,0 +1,29 @@
+﻿const string input = "<div><h2>Widgets &trade;</h2><span>5000</span></div>";
+
+string quantity = "";
+string output = "";
+
+// Your work here
+
+string[] divElements = { "<div>", "</div>" };
+string[] spanElements = { "<span>", "</span>" };
+
+int openingDivPosition = input.IndexOf(divElements[0]);
+int closingDivPosition = input.IndexOf(divElements[1]);
+
+int openingSpanPosition = input.IndexOf(spanElements[0]);
+int closingSpanPosition = input.IndexOf(spanElements[1]);
+
+output = input.Substring(
+    openingDivPosition + divElements[0].Length,
+    closingDivPosition - (openingDivPosition + divElements[0].Length)
+);
+output = output.Replace("&trade;", "&reg");
+
+quantity = input.Substring(
+    openingSpanPosition + spanElements[0].Length,
+    closingSpanPosition - (openingSpanPosition + spanElements[0].Length)
+);
+
+Console.WriteLine($"Quantity: {quantity}");
+Console.WriteLine($"Output: {output}");
